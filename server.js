5151f6018e71762544ce53fd9f6f4ade200b1dfc
Manoj <rudranani12897@gmail.com>
2025-12-14 17:39:02 +0530

    Remove .env from repo and ignore it

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
+dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 const app = express();
